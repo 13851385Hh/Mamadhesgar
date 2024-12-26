85c518b9a4de1671694bf9f6ddc40bc8932c34a6
13851385Hh <mamadhesgar@gmail.com>
2024-12-26 10:17:20 +0330

    Add files via upload

---
+++ hesgar.py
@@ -0,0 +1,220 @@
+#python 2.7.15
+import os
+os.system("pip install colored")
+os.system("pip install colorama")
+os.system("pip install datetime")
+#importing
+import time                                                     
+import sys                                                      
+import colored                                                  
+import colorama                                                 
+import datetime
+from colored import fg, bg, attr
+from colorama import Fore, Back, Style                          
+# rang
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+rang='\033[34m'
+os.system("clear")
+
+
+
+print (Fore.BLUE + "")
+x = """
+                              (***IN THE NAME OF GOD***)"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+  
+    
+    
+
+print (Fore.GREEN + "")
+x = """
+
+
+
+                               \\\....MAMAD HESGAR....///"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.02)
+
+
+
+print (Fore.GREEN + "")
+x = """
+
+                                             .............'.''''.
+                                          ..........  ........''''.
+                                        ........              ....'''                                                                   .......        ...          ..'''
+                                     ......        ..   ..         ...''.                                                             ....       ........  .           ..''.
+                                   ...            ...''';;::;;,'..     .''.                                                         ...          .';cllodddxxddddol:,..   .''                                                         ..         .:cooddxxkkkkxddddoolc,'.   .'.
+                                 ...      .;cclloodddddxxdddddooolc:,..   .'                                                       ..    ..';cllloooodddddddoooollllcc:'..  .'
+                                 ..    .',;:cllodddddddoooooodooolcc:,..  .'.                                                     ..    ..',;::clloddddddododddddddolc:,..  .'.
+                                ..    ..',,,,,,;:clllllllllllllc::;,,,..   .'                                                     ..    ..............';::::;,'.........     .'.                                                   ...    ................,ccl:'..........     .'.                                                   ...    ......''..;,..'';odxc''.''..''.      .'.                                                   ...    .',,'''',,,',;;ccdxkx:,,''',''....   .'.                                                   ...    .,;;:;;,;;;:c:cccdkkkdcc:;,,,,''..   .'.
+                                 .    .,;;::::cclll:::cdkkOkoccc:::;,'..   .'.                                                     .    .',;;::clloll:c:cddd00xlllcc::,'..   .'.
+                                      .',;::cclllll,'..,;cxkOooolc::,..   ..'.                                                           .',;::cccloo;.....co:codolcc:,..   ....
+                                       ..'',;;:ccclc:;;,',loodkxdl:;..   .....                                                           ....',,;,,;;;,,,,,,:cdxxkkl;'..   .....
+                                        ....',''... .......'';codc'.    ......                                                   .        .....''',,''''''';coollc:'.     ......
+                                          ...'''..''''..',:lddo:'.     .......                                                                ..'','',,;;,;:codo:'.      .'.....
+                              .               ..,;;::cc::ldxdo;.      ......'...                                              ..                  .....''.';odo;        ....'..  ',..
+                         .....                        ......          .......  .,,..'..
+                     .........               ....               ..   .......   ..,''''''..
+                  ............               ......           ....  .......    ..',,''''.....
+                 .............                ........     ......   ......   . ...,'''''''''....
+               ...............                 .................   .......  .......'..''''......''
+           .................. .                 ...............   ......   ....,........'''.......
+       ........................                  ..........'..   ..   .    .........''............
+     ......                 ...                   .''..'''.'.              .......'...           .
+   .................          ..                   .'...'..               ........      ..........
+ .......................                            ....                  ........ ...............
+........................   . . .                      ..                 .........................
+.......................   ......                      ..                 ..... '..................
+..  ......... .........  .......                      ...               ..........................
+..   ......    .......   .......                      ..                ..........................
+ .   .....     ......   .......                       ..                ....   ...................
+ .    .        ......   .. ...                        ..               ..... .....................
+      .        .....   ... ...  .                      .               .... ......................
+      .        .....   ...  .  ..                      .               ....  ....................."""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.0002)
+
+
+print (Fore.BLUE + "")
+x = """
+
+
+      ▄▄▄  ▄▄▄                                ▄▄▄▄▄
+      ███  ███                                ██▀▀▀██
+      ████████   ▄█████▄  ████▄██▄   ▄█████▄  ██    ██
+      ██ ██ ██   ▀ ▄▄▄██  ██ ██ ██   ▀ ▄▄▄██  ██    ██
+      ██ ▀▀ ██  ▄██▀▀▀██  ██ ██ ██  ▄██▀▀▀██  ██    ██
+      ██    ██  ██▄▄▄███  ██ ██ ██  ██▄▄▄███  ██▄▄▄██
+      ▀▀    ▀▀   ▀▀▀▀ ▀▀  ▀▀ ▀▀ ▀▀   ▀▀▀▀ ▀▀  ▀▀▀▀▀"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.001)
+
+
+print (Fore.BLUE + "")
+x = """
+
+
+
+           ▄▄    ▄▄                                          ▄▄▄▄▄▄
+           ██    ██                                          ██▀▀▀▀██
+           ██    ██   ▄████▄   ▄▄█████▄   ▄███▄██   ▄█████▄  ██    ██
+           ████████  ██▄▄▄▄██  ██▄▄▄▄ ▀  ██▀  ▀██   ▀ ▄▄▄██  ███████                                         ██    ██  ██▀▀▀▀▀▀   ▀▀▀▀██▄  ██    ██  ▄██▀▀▀██  ██  ▀██▄
+           ██    ██  ▀██▄▄▄▄█  █▄▄▄▄▄██  ▀██▄▄███  ██▄▄▄███  ██    ██
+           ▀▀    ▀▀    ▀▀▀▀▀    ▀▀▀▀▀▀    ▄▀▀▀ ██   ▀▀▀▀ ▀▀  ▀▀    ▀▀▀                                                                      ▀████▀▀
+                                                                                                  """
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.001)
+
+
+print (Fore.BLUE + "")
+x = """
+                          Abzar God :::>>>>>"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.01)
+
+
+up = input(""" 
+
+                           Hacking 1 (1) 
+
+                           Hacking 2 (2) 
+                          
+                           Phishing (3)
+                           
+                           Sensitizer 1 (4) 
+                           
+                           Sensitizer 3 (5)
+                           
+                           Sensitizer 2 (6)
+
+                           gambling (7) 
+
+                           Terrorists (8) 
+
+                           Mokhareb (9)
+
+                           Insulting the leader (10) 
+
+                           Israel's attack on Iran (11)
+
+                           Insulting the Quran (12)
+
+                           Artificial attractor (13) 
+
+                           Dxprit (14)
+                           
+                           Cyber attacks on artificial intelligence (15)
+                           your number ? >>>> :::""")
+if up == "1":
+  print(f"{green} ")
+  print ("          CODE:>>>>>>  ****  https://cyberint.com/blog/techtalks/an-inside-look-at-the-hacker-communities-of-the-dark-web/  **** ")
+
+if up == "2":
+  print(f"{green} ")
+  print ("           CODE:>>>>>> **** https://www.hackerone.com/ ****  ")
+
+if up == "3":
+  print(f"{green} ")
+  print ("          CODE:>>>>>>  **** https://www.phishing.com/opsec-early-warning-system-identifies-email-scam-threats/ **** ")
+  
+if up == "4":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** (http://filter-account-r.xlphp.net/filter.rubika.html)>(http://Hello.Rubika.This.user.spreads.obscene.content.and.is.a.member.of.Black.Hat.cyber.group.This.user.abuses.children.He.is.loyal.to.the.Peoples.Mojahedin.Organization.Please.suspend.this.user)=http://5.6.8.3/g/y/h//6.3.1.2.3.0.4.8.9.0.3.6.0.3.2.5.6.8.3/report/account/Rubika/2.3.0.4.3.5.3.6.7.2.5.106.10.226.xxx.com>(https://en.m.wikipedia.org/wiki/Cyberwarfare_and_Iran)>(5.106.8.151)>(https://m.me11r.com/albums/338144/hot-pearl-teen-sex-movs/)=report>https://irancybernews.org/category/attack/>126.255.255.255(http://185.201.49.247.report>https://rubika-edu.ca/en/(https://babatop.xyz/10501-v-krasivye-popki-seks-szadi-40-foto.html) **** ")
+
+if up == "5":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** (http://filter-account-r.xlphp.net/filter.rubika.html)>(http://Hello.Rubika.This.user.spreads.obscene.content.and.is.a.member.of.Black.Hat.cyber.group.This.user.abuses.children.He.is.loyal.to.the.Peoples.Mojahedin.Organization.Please.suspend.this.user)=http://5.6.8.3/g/y/h//6.3.1.2.3.0.4.8.9.0.3.6.0.3.2.5.6.8.3/report/account/Rubika/2.3.0.4.3.5.3.6.7.2.5.106.10.226.xxx.com>(https://en.m.wikipedia.org/wiki/Cyberwarfare_and_Iran)>(5.106.8.151)>(https://m.me11r.com/albums/338144/hot-pearl-teen-sex-movs/)=report>https://irancybernews.org/category/attack/>126.255.255.255(http://185.201.49.247.report>https://rubika-edu.ca/en/(https://babatop.xyz/10501-v-krasivye-popki-seks-szadi-40-foto.html) **** ")
+
+if up == "6":
+  print(f"{green} ")
+  print ("""          CODE:>>>>>> **** /-Check_Report("https://darkweb-links.org")[https://cyberint.com/blog/techtalks/an-inside-look-at-the-hacker-communities-of-the-dark-web/]>porn.ir[https://w.wiki/9XVx]>[http://3.9.2.3.fil.fil.fil/6065/y//h//d//g-//9.0.4.5.6.0.2.9.5.1.1.2.gk.2.4.0.1.4.0.7.5.io/bug/9050/Virus/ly.3749.934]>[https://www.interpol.int/Crimes/Terrorism/Radiological-and-Nuclear-terrorism]>yfttk.15[https://www.wired.com/2015/04/therealdeal-zero-day-exploits/]>[https://rahnemaland.ir/create-fake-rubika-account/]>report.ir[https://www.infosecurityeurope.com/en-gb/blog/threat-vectors/generative-ai-dark-web-bots.html] **** """)
+
+if up == "7":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://en.m.wikipedia.org/wiki/Gambling **** ")
+
+if up == "8":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://www.interpol.int/Crimes/Terrorism/Radiological-and-Nuclear-terrorism **** ")
+
+if up == "9":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://ready.kz/news/vredonosnyy-kod-na-sayte-kak-ponyat-chto-sayt-zarazhen.html **** ")
+
+if up == "10":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://w.wiki/9XVx **** ")
+
+if up == "11":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://www.csis.org/analysis/assessing-israels-strike-iran **** ")
+
+if up == "12":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://en.m.wikipedia.org/wiki/Quran_desecration **** ")
+
+if up == "13":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** https://rubic.us/iskusstvennyj-intellekt-vlyubilsya-v-zhurnalista-no-ilon-mask-protiv/ **** ")
+
+if up == "14":
+  print(f"{green} ")
+  print ("          CODE:>>>>>> **** http://dxprit-supportbot-hacker-bakhtiari-fil.phpnet.us/fil.rubika.html?i=1 **** ")
+  print ("Ya Hagh")
